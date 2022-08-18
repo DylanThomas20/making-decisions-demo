2c31ace835c36f938fab7414c82b5dc57b1f25c6
DylanThomas20 <dtsports17@gmail.com>
2022-08-18 12:55:57 -0600

    Maybe Complete

--- arrays.js
+++ arrays.js
@@ -1,4 +1,26 @@
-/*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
-*/
+
+
+let backpack = []
+
+backpack.push('Jedi Robe')
+backpack.push('Lightsaber')
+backpack.push('Crippling sense of failure and guilt')
+
+backpack.splice(1,1)
+
+backpack.push('Wookie Coat')
+
+backpack.pop()
+
+backpack.push('blue milk', 'bady yoda','a child named luke', 'com device', 'high ground', 'blaster')
+
+let stroller = backpack.splice(3,2)
+
+// console.log(backpack)
+// console.log(stroller)
+
+// console.log(backpack.length)
+
+for(let i=0; i < backpack.length; i++){
+    console.log(backpack[i])
+}
